a81539e0a7f6804d54a0f16f6cdc9abc355f1089
Theresa <theresa.sunitsch@hotmail.com>
2023-04-28 14:24:18 +0200

    push

---
+++ concatenation.py
@@ -0,0 +1,26 @@
+x = "Python is "
+y = "awesome."
+
+z = x + y
+print(z)
+
+u = 5
+v = 4
+z = u + v
+print(z)
+
+z = u / v
+print(z)
+
+q = u / v
+print(q)
+
+modulus = u % v
+print(modulus)
+
+exponentiation = u ** v
+print(exponentiation)
+
+#floordevision
+floordevision = u // v
+print(floordevision)
